fe5511c81f8b0f73a904697131ce91175daa3d05
Shamil Roshan.N <shamilroshan390@gmail.com>
2026-08-10 22:22:09 +0530

    Navbar and footer are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
+
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    )
 }
 
 export default App
